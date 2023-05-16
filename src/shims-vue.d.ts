de04809927ea9e4b786c2c40fdea823873977e18
Nishant Shah <nishantcshah13@gmail.com>
2023-05-16 16:57:58 +0530

    store

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,4 +1,11 @@
 /* eslint-disable */
+//import { Store } from '@/data/store';
+
+// declare module '@vue/runtime-core' {
+//   interface ComponentCustomProperties {
+//     $store: Store;
+//   }
+// }
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
