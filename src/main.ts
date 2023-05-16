883485b93cf1c317609f8c18afb82a3401c5a72d
Nishant Shah <nishantcshah13@gmail.com>
2023-05-16 14:28:36 +0530

    init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './presentation/router'
+import store from './data/store'
 
 createApp(App).use(store).use(router).mount('#app')
