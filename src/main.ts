ed7ba1d9865a323e888e6222eec244fb877c3634
Nishant Shah <nishantcshah13@gmail.com>
2023-05-16 14:43:55 +0530

    login page

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './presentation/router'
 import store from './data/store'
+import '@/presentation/assets/main.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 createApp(App).use(store).use(router).mount('#app')
---
+++ src/presentation/views/LoginView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="login">
+      <LoginForm/>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { Options, Vue } from 'vue-class-component';
+  import LoginForm from '@/presentation/components/LoginForm.vue'; // @ is an alias to /src
+  
+  @Options({
+    components: {
+        LoginForm,
+    },
+  })
+  export default class LoginView extends Vue {}
+  </script>
+  
